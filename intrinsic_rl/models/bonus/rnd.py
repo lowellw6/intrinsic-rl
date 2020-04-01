b6d6c93c9448b7d4e3bcef0e224a6b4750e674e0
Lowell Weissman <lowell6@vt.edu>
2020-03-31 23:07:06 -0400

    added intrinsic reward normalization to RND model

--- intrinsic_rl/models/bonus/rnd.py
+++ intrinsic_rl/models/bonus/rnd.py
@@ -22,12 +22,13 @@ class RndBonusModule(SelfSupervisedModule):
             ):
         """
         Constructs target and distillation model. Assumes identical architectures.
-        Also constructs observation normalization model.
+        Also constructs normalization models for observation and intrinsic rewards.
         """
         super().__init__()
         self.target_model = RndCls(**rnd_model_kwargs)
         self.distill_model = RndCls(**rnd_model_kwargs)
-        self.obs_norm_model = RunningMeanStdModel(rnd_model_kwargs["input_shape"])  # Requires RndCls takes input_shape
+        self.obs_rms = RunningMeanStdModel(rnd_model_kwargs["input_shape"])  # Requires RndCls takes input_shape
+        self.int_rew_rms = RunningMeanStdModel(torch.Size([1]))
 
     def normalize_obs(self, obs):
         """
@@ -35,14 +36,33 @@ class RndBonusModule(SelfSupervisedModule):
         https://arxiv.org/abs/1810.12894. This is necessary since the target
         network is fixed and cannot adjust to varying environments.
 
+        This model should be initialized in the sampler by running
+        a small number of observations through it.
+
         WARNING: If observations are already normalized using
-        a different model / formulation, this can cause issues
-        if this model is initialized in the sampler.
+        a different model / formulation, this will cause issues
+        if this model is initialized on raw obs in the sampler.
         """
-        self.obs_norm_model.update(obs.to(dtype=torch.float32))
-        obs = (obs - self.obs_norm_model.mean) / (self.obs_norm_model.var.sqrt() + 1e-8)
+        self.obs_rms.update(obs.to(dtype=torch.float32))  # Obs may be byte tensor (e.g. 8-bit pixels)
+        obs = (obs - self.obs_rms.mean) / (self.obs_rms.var.sqrt() + 1e-8)
         obs = torch.clamp(obs, min=-5, max=5)
         return obs
+
+    def normalize_int_rew(self, int_rew):
+        """
+        Normalizes intrinsic rewards according to specifications in
+        https://arxiv.org/abs/1810.12894. This is done to remove the
+        need to search for optimal intrinsic reward scaling factors in between
+        different environments.
+
+        This model is *not* expected to be initialized, if following the authors'
+        implementation.
+        """
+        batch_size = int_rew.numel()
+        int_rew = int_rew.view((batch_size, 1))  # Ensures a non-batch dimension for RunningMeanStdModel
+        self.int_rew_rms.update(int_rew)
+        int_rew /= self.int_rew_rms.var.sqrt()
+        return int_rew.squeeze()
 
     def forward(self, obs):
         """
@@ -55,5 +75,5 @@ class RndBonusModule(SelfSupervisedModule):
         target_feat, _ = self.target_model(obs)
         pred_errors = torch.mean((distill_feat - target_feat.detach()) ** 2, dim=-1)  # Maintains batch dimension
         distill_loss = torch.mean(pred_errors)  # Reduces batch dimension
-        int_rew = pred_errors.detach()
+        int_rew = self.normalize_int_rew(pred_errors.detach())
         return int_rew, distill_loss
